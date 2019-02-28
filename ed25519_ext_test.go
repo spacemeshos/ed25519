6ef3233e9a26ee04514268018ae29c13d8abcd85
Aviv Eyal <aviveyal07@gmail.com>
2019-02-28 21:27:40 +0200

    Move VerifyEx from test file

--- ed25519_ext_test.go
+++ ed25519_ext_test.go
@@ -5,58 +5,9 @@ package ed25519
 
 import (
 	"bytes"
-	"crypto/sha512"
-	"github.com/spacemeshos/ed25519/internal/edwards25519"
 	"github.com/stretchr/testify/assert"
-	"strconv"
 	"testing"
 )
-
-func VerifyEx(publicKey PublicKey, message, sig []byte) bool {
-	if l := len(publicKey); l != PublicKeySize {
-		panic("ed25519: bad public key length: " + strconv.Itoa(l))
-	}
-
-	if len(sig) != SignatureSize || sig[63]&224 != 0 {
-		return false
-	}
-
-	var A edwards25519.ExtendedGroupElement
-	var publicKeyBytes [32]byte
-	copy(publicKeyBytes[:], publicKey)
-	if !A.FromBytes(&publicKeyBytes) {
-		return false
-	}
-	edwards25519.FeNeg(&A.X, &A.X)
-	edwards25519.FeNeg(&A.T, &A.T)
-
-	h := sha512.New()
-	h.Write(sig[:32])
-	// we remove the public key from the hash
-	// h.Write(publicKey[:])
-	h.Write(message)
-	var digest [64]byte
-	h.Sum(digest[:0])
-
-	var hReduced [32]byte
-	edwards25519.ScReduce(&hReduced, &digest)
-
-	var R edwards25519.ProjectiveGroupElement
-	var s [32]byte
-	copy(s[:], sig[32:])
-
-	// https://tools.ietf.org/html/rfc8032#section-5.1.7 requires that s be in
-	// the range [0, order) in order to prevent signature malleability.
-	if !edwards25519.ScMinimal(&s) {
-		return false
-	}
-
-	edwards25519.GeDoubleScalarMultVartime(&R, &hReduced, &A, &s)
-
-	var checkR [32]byte
-	R.ToBytes(&checkR)
-	return bytes.Equal(sig[:32], checkR[:])
-}
 
 func TestPublicKeyExtraction(t *testing.T) {
 	var zero zeroReader
